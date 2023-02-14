5e14ec782289ba618a8dc6f31e85508a152627d6
RyanNolan1 <114003965+RyanNolan1@users.noreply.github.com>
2023-02-14 17:58:20 +0000

    this will add the function to display the stations and also sort them by the distance from the post code

--- index.js
+++ index.js
@@ -1,17 +1,104 @@
-const axios = require("axios");
+import { api_key } from "./key.js";
 
-axios
-  .get(
-    "https://uk1.ukvehicledata.co.uk/api/datapackage/FuelPriceData?v=2&api_nullitems=1&auth_apikey=POSTCODE=BS12AN"
+document
+  .getElementById("submit-postcode")
+  .addEventListener("click", getPostCode);
+
+document
+  .getElementById("sort-by-distance")
+  .addEventListener("click", sortByDistance);
+
+export function getPostCode() {
+  const postCode = document.getElementById("post-code-input").value;
+
+  return fetch(
+    `https://uk1.ukvehicledata.co.uk/api/datapackage/FuelPriceData?v=2&api_nullitems=1&auth_apikey=${api_key}&key_POSTCODE=${postCode}`
   )
-  .then(function (response) {
-    // handle success
-    console.log(response.data.Response.DataItems.FuelStationDetails);
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
-  .then(function () {
-    // always executed
-  });
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error("NETWORK RESPONSE ERROR");
+      }
+    })
+    .then((data) => {
+      displayStations(data);
+    })
+    .catch((error) => console.error("FETCH ERROR:", error));
+
+  function displayStations(data) {
+    let results = data.Response.DataItems.FuelStationDetails.FuelStationList;
+
+    let table = `
+        <thead>
+        <tr>
+        <th>Price</th>
+        <th>Company</th>
+        <th>Distance</th>
+        <th>Location</th>
+        </tr>
+         </thread>`;
+
+    for (let i = 0; i < results.length; i++) {
+      table += `
+    <tbody>
+    <tr>
+<td>${results[i].FuelPriceList[1].LatestRecordedPrice.InPence} </td>
+<td>${results[i].Brand}</td>
+<td>${results[i].DistanceFromSearchPostcode}</td>
+<td>${results[i].Suburb ? results[i].Suburb : results[i].Town}</td>
+</tr>
+</tbody>`;
+    }
+    document.getElementById("fuel-table").innerHTML = table;
+  }
+}
+
+export function sortByDistance() {
+  const postCode = document.getElementById("post-code-input").value;
+
+  return fetch(
+    `https://uk1.ukvehicledata.co.uk/api/datapackage/FuelPriceData?v=2&api_nullitems=1&auth_apikey=${api_key}&key_POSTCODE=${postCode}`
+  )
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error("NETWORK RESPONSE ERROR");
+      }
+    })
+    .then((data) => {
+      orderByDistance(data);
+    })
+    .catch((error) => console.error("FETCH ERROR:", error));
+
+  function orderByDistance(data) {
+    let results = data.Response.DataItems.FuelStationDetails.FuelStationList;
+    results.sort((a, b) => {
+      return a.DistanceFromSearchPostcode - b.DistanceFromSearchPostcode;
+    });
+
+    let table = `
+    <thead>
+    <tr>
+    <th>Price</th>
+    <th>Company</th>
+    <th>Distance</th>
+    <th>Location</th>
+    </tr>
+     </thread>`;
+
+    for (let i = 0; i < results.length; i++) {
+      table += `
+    <tbody>
+    <tr>
+<td>${results[i].FuelPriceList[1].LatestRecordedPrice.InPence} </td>
+<td>${results[i].Brand}</td>
+<td>${results[i].DistanceFromSearchPostcode}</td>
+<td>${results[i].Suburb ? results[i].Suburb : results[i].Town}</td>
+</tr>
+</tbody>`;
+    }
+    document.getElementById("fuel-table").innerHTML = table;
+  }
+}
